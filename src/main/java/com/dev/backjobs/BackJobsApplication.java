ae6b6d54431814bf9e4a599f271febead708079e
dev-praveen <spraveen0244@gmail.com>
2024-06-26 19:27:02 +0530

    refactor[logging]: added jobrunrlog

--- src/main/java/com/dev/backjobs/BackJobsApplication.java
+++ src/main/java/com/dev/backjobs/BackJobsApplication.java
@@ -1,14 +1,18 @@
 package com.dev.backjobs;
 
-import lombok.extern.slf4j.Slf4j;
 import org.jobrunr.jobs.annotations.Job;
+import org.jobrunr.jobs.context.JobRunrDashboardLogger;
 import org.jobrunr.scheduling.BackgroundJob;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Slf4j
 @SpringBootApplication
 public class BackJobsApplication {
+
+  private static final Logger log =
+      new JobRunrDashboardLogger(LoggerFactory.getLogger(BackJobsApplication.class));
 
   public static void main(String[] args) {
     SpringApplication.run(BackJobsApplication.class, args);
@@ -20,14 +24,16 @@ public class BackJobsApplication {
   @Job(name = "simple job task", retries = 2)
   public static void executeSomeTask() throws InterruptedException {
 
-    Thread.sleep(30000);
     log.info("Simple Job is executing.......{}", Thread.currentThread().getName());
+    Thread.sleep(30000);
+    log.info("Simple Job is executed.......{}", Thread.currentThread().getName());
   }
 
   @Job(name = "complex job task", retries = 2)
   public static void executeComplexTask() throws InterruptedException {
 
-    Thread.sleep(300000);
     log.info("Complex Job is executing.......{}", Thread.currentThread().getName());
+    Thread.sleep(300000);
+    log.info("Complex Job is executed.......{}", Thread.currentThread().getName());
   }
 }
